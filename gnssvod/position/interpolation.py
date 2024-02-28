5c7e39c58655bce5c6f657ef5c0fe7206dc6c6d7
vincenthumphrey <vincent.humphrey@env.ethz.ch>
2024-02-28 08:49:41 +0100

    bugfix and clean up

--- gnssvod/position/interpolation.py
+++ gnssvod/position/interpolation.py
@@ -4,6 +4,7 @@ import time
 import pandas as _pd
 import numpy as _np
 import pdb
+import datetime as _dt
 from datetime import datetime, timedelta
 from gnssvod.io import readFile
 from gnssvod.geodesy.coordinate import cart2ell, geocentric_latitude
@@ -13,32 +14,27 @@ from gnssvod.position.position import _observation_picker_by_band
 
 __all__ = ["sp3_interp", "ionosphere_interp"]
 
-def sp3_interp_fast(epoch, interval=30, poly_degree=16, sp3_product="gfz", clock_product="gfz"):
+def sp3_interp_fast(start_time, end_time, interval=30, poly_degree=16, sp3_product="gfz", clock_product="gfz"):
+    # add a large buffer around start and end time
+    start_time_withbuff = start_time-_dt.timedelta(hours=23)
+    end_time_withbuff = end_time+_dt.timedelta(hours=23)
     # determine all required orbit and clock files
-    epoch_yesterday = epoch - timedelta(days=1)
-    epoch_tomorrow =  epoch + timedelta(days=1)
-    yesterday = sp3FileName(epoch_yesterday, sp3_product)
-    today = sp3FileName(epoch, sp3_product)
-    tomorrow = sp3FileName(epoch_tomorrow, sp3_product)
-    clockFile = clockFileName(epoch, interval, clock_product)
+    start_epoch = _dt.date(start_time_withbuff.year,start_time_withbuff.month,start_time_withbuff.day)
+    end_epoch = _dt.date(end_time_withbuff.year,end_time_withbuff.month,end_time_withbuff.day)
+    epochs = _pd.date_range(start_epoch,end_epoch,freq='d')
+    sp3FileNames = [sp3FileName(x.date(),sp3_product) for x in epochs]
+    clockFileNames = [clockFileName(x.date(),interval,clock_product) for x in epochs]
     #--------------------------------------------------------------------------
     # reading all those files, downloading them if necessary
-    yes   = readFile.read_sp3File(yesterday) 
-    tod = readFile.read_sp3File(today) 
-    tom   = readFile.read_sp3File(tomorrow) 
-    clock = readFile.read_clockFile(clockFile)
+    sp3 = [readFile.read_sp3File(x) for x in sp3FileNames]
+    clock = [readFile.read_clockFile(x) for x in clockFileNames]
     #--------------------------------------------------------------------------
     # concatenate data from current day and some buffer before and after
     start = time.time()
-    yes = yes.dropna(subset=["deltaT"])
-    tod = tod.dropna(subset=["deltaT"])
-    tom = tom.dropna(subset=["deltaT"])
-    yes = yes.reorder_levels(["Epoch","SV"])
-    tod = tod.reorder_levels(["Epoch","SV"])
-    tom = tom.reorder_levels(["Epoch","SV"])
-    yes = yes.loc[(slice(_pd.Timestamp(datetime(epoch_yesterday.year, epoch_yesterday.month, epoch_yesterday.day,23,0,0)),_pd.Timestamp(datetime(epoch_yesterday.year, epoch_yesterday.month, epoch_yesterday.day,23,59,59))))]
-    tom = tom.loc[(slice(_pd.Timestamp(datetime(epoch_tomorrow.year, epoch_tomorrow.month, epoch_tomorrow.day,0,0,0)),_pd.Timestamp(datetime(epoch_tomorrow.year, epoch_tomorrow.month, epoch_tomorrow.day,3,0,0))))]
-    sp3 = _pd.concat([yes,tod,tom], axis=0)
+    sp3 = _pd.concat(sp3,axis=0)
+    clock = _pd.concat(clock,axis=0)
+    sp3 = sp3.dropna(subset=["deltaT"]).reorder_levels(["Epoch","SV"])
+    sp3 = sp3.loc[(slice(start_time_withbuff,end_time_withbuff))]
     #--------------------------------------------------------------------------
     svList = sp3.index.get_level_values("SV").unique()
     svList = svList.sort_values()
